379ae1f44888adb5cb437c8eb8d656edb8c717cb
Aser Granado Amores <agranadoa09@informatica.iesvalledeljerteplasencia.es>
2023-05-10 20:29:20 +0200

    Modificar Perfil Activity - Solicitud de permisos

--- app/src/main/java/com/example/teleappsistencia/ModificarPerfilActivity.java
+++ app/src/main/java/com/example/teleappsistencia/ModificarPerfilActivity.java
@@ -1,22 +1,31 @@
 package com.example.teleappsistencia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.teleappsistencia.modelos.Usuario;
 import com.example.teleappsistencia.utilidades.Constantes;
 import com.example.teleappsistencia.utilidades.Utilidad;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 /**
  * Activity invocado al pulsar el icono de la imagen de perfil de un usuario.
  * Este activity le permitirá al usuario modificar su imagen de perfil, entre otras cosas.
@@ -30,6 +39,12 @@ public class ModificarPerfilActivity extends AppCompatActivity {
     TextView tvUsername;
     EditText edtNombre, edtApellidos, edtEmail;
 
+    // Permisos necesarios
+    private static final List<String> REQUIRED_PERMISSIONS = Arrays.asList(
+        Manifest.permission.READ_EXTERNAL_STORAGE,
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
+    );
+    private boolean tienePermisos = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +61,34 @@ public class ModificarPerfilActivity extends AppCompatActivity {
 
         // Conectar acciones
         btnGuardar.setOnClickListener(_v -> enviarDatos());
+    }
+
+    /**
+     * Solicita al usuario los permisos necesarios que no tengamos ya.
+     * @return Ddevuelve true solo si no se han tenido que pedir permisos.
+     */
+    private boolean pedirPermisos() {
+        if (!tienePermisos) {
+            List<String> missing = new ArrayList<>();
+
+            // Anotar los permitos que faltan / si faltan
+            for (String permission : REQUIRED_PERMISSIONS) {
+                int permissionCheck = ActivityCompat.checkSelfPermission(this, permission);
+                if (PackageManager.PERMISSION_DENIED == permissionCheck ) {
+                    missing.add(permission);
+                }
+            }
+
+            tienePermisos = missing.isEmpty();
+            // Si hay permisos que nos falten, solicitarlos
+            if (!tienePermisos) {
+                String[] _missing = new String[missing.size()];
+                missing.toArray(_missing);
+                ActivityCompat.requestPermissions(this, _missing, 255);
+            }
+        }
+
+        return tienePermisos;
     }
 
     /**
@@ -96,5 +139,21 @@ public class ModificarPerfilActivity extends AppCompatActivity {
 
         // Cerrar la activity
         finish();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 255) {
+            List<Integer> grantResultsList = Arrays.stream(grantResults)
+                    .boxed().collect(Collectors.toList());
+
+            // Si se ha procesado algúna petición de permisos, y no se nos ha denegado ninguno
+            if (!grantResultsList.isEmpty() && !grantResultsList.contains(PackageManager.PERMISSION_DENIED)) {
+                this.tienePermisos = true;
+            } else {
+                Toast.makeText(this, Constantes.TOAST_MODPERFIL_PERMISO_NECESARIO, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
